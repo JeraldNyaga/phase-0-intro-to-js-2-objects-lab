b184b8aaed40714299a238960b0aa0b624335a38
judgeOstrich <jeraldnyaga15@gmail.com>
2025-03-03 02:54:50 +0300

    Add destructive and non-destructive object functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "",
+    streetAddress: "",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {...obj, [key]:value}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value;
+    return obj
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
